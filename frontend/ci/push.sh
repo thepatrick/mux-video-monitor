92adaed8acd07d08cd7feb2f5225f3bd32e02ff9
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-09-17 14:36:31 +1000

    Begin publishing frontend (add index.html)

---
+++ frontend/ci/push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+cd $(dirname "$0")/..
+
+. ../ci/functions.sh
+
+start_group "Installing dependencies"
+# npm ci
+echo "None yet"
+end_group
+
+start_group "Building"
+# npm run clean
+# npm run build
+mkdir dist
+cp index.html dist/index.html
+end_group
+
+start_group "Uploading to s3://$FRONTEND_BUCKET"
+aws s3 sync dist/ "s3://${FRONTEND_BUCKET}/"
+end_group
