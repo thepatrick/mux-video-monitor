4876a649ef733065c6be112c2ef13b4828b2f859
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-10-05 17:21:25 +1100

    attempt initial build to s3

---
+++ infra/backend-bucket/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+cd $(dirname "$0")/..
+
+export AWS_DEFAULT_REGION=ap-southeast-2
+
+STACK_NAME=mux-video-monitor-backend-bucket
+
+aws cloudformation deploy \
+  --template-file backend-bucket.cfn.yaml \
+  --stack-name "$STACK_NAME"
+
+LAMBDA_BUCKET=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query 'Stacks[0].Outputs[?OutputKey==`BackendBucket`].OutputValue' --output text)
+
+echo "::set-output name=backendBucket::$LAMBDA_BUCKET"
