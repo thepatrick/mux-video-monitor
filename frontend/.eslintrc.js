6c3dfb5faa7f1616a19a662190d2f79f903ebc9a
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-10-05 17:21:40 +1100

    some grab bag things

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -16,4 +16,22 @@ module.exports = {
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
   root: true,
+  overrides: [
+    {
+      files: ['**/*.test.ts'],
+      env: {
+        jest: true, // now **/*.test.js files' env has both es6 *and* jest
+      },
+      // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
+      // "extends": ["plugin:jest/recommended"]
+      plugins: ['jest'],
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+      },
+    },
+  ],
 };
