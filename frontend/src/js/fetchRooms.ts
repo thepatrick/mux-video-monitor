4680c14de06f32a49458d6ccbac7ca576654f894
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-10-29 22:43:07 +1100

    fetch streams #8

--- frontend/src/js/fetchRooms.ts
+++ frontend/src/js/fetchRooms.ts
@@ -15,17 +15,41 @@ interface FetchRoomsError {
   error: string;
 }
 
+interface FetchRoomsResponseRoom {
+  id: string;
+  tags: Record<string, string>;
+}
+
+interface FetchRoomsResponseOk {
+  ok: true;
+  rooms: FetchRoomsResponseRoom[];
+}
+
+interface FetchRoomsResponseError {
+  ok: false;
+  error: string;
+}
+
 export const fetchRooms = async (): Promise<FetchRoomsOk | FetchRoomsError> => {
   try {
     const before = Date.now();
-    const fetchResponse = await fetch(`/api/rooms.json?${nanoid()}`);
-    const rooms = (await fetchResponse.json()) as Room[];
+    const fetchResponse = await fetch(`/api/stream?${nanoid()}`);
+    const rooms = (await fetchResponse.json()) as FetchRoomsResponseOk | FetchRoomsResponseError;
+
+    if (rooms.ok === false) {
+      throw new Error(rooms.error);
+    }
+
+    const r = rooms.rooms.map((room) => ({
+      id: room.id,
+      name: room.tags['multiview:title'] || room.id,
+    }));
 
     const totalTime = Date.now() - before;
 
     console.log(`[Rooms] Got rooms in ${totalTime}ms`, rooms);
 
-    return { ok: true, rooms };
+    return { ok: true, rooms: r };
   } catch (err) {
     return { ok: false, error: (err as Error).message };
   }
