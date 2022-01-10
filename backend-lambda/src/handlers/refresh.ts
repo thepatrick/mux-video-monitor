0ed2d35f3a6f95e171961135c7cfb73387fa5f32
Patrick Quinn-Graham <make-contact@pftqg.com>
2022-01-10 17:29:25 +1100

    Add refresh fn #33 (#38)
    * Add refresh fn #33
    
    * Add refresh fn (fix description) #33

--- backend-lambda/src/handlers/refresh.ts
+++ backend-lambda/src/handlers/refresh.ts
@@ -7,12 +7,12 @@ import { getRoomsFromDynamo } from './rooms/getRoomsFromDynamo';
 
 export const TableName = process.env.CACHE_TABLE_NAME;
 
-export const listRooms: APIGatewayProxyHandlerV2 = catchErrors(async (event, context) => {
+export const refresh: APIGatewayProxyHandlerV2 = catchErrors(async (event, context) => {
   if (!TableName) {
     throw new Error('CACHE_TABLE_NAME not set');
   }
 
-  const maybeRooms = await getRoomsFromDynamo(TableName, false);
+  const maybeRooms = await getRoomsFromDynamo(TableName, true);
 
   if (isFailure(maybeRooms)) {
     throw maybeRooms.value;
@@ -24,13 +24,13 @@ export const listRooms: APIGatewayProxyHandlerV2 = catchErrors(async (event, con
   });
 });
 
-if (process.env.TEST_HANDLER === 'getRoomsFromDynamo') {
+if (process.env.TEST_HANDLER === 'refreshDynamoCache') {
   if (!TableName) {
     throw new Error('CACHE_TABLE_NAME not set');
   }
 
   console.log('Ok, lets do this');
-  void getRoomsFromDynamo(TableName, false).then(
+  void getRoomsFromDynamo(TableName, true).then(
     (results) => console.log('[DONE]', results),
     (err) => console.log('[ERROR]', err),
   );
