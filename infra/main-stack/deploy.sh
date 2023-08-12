c64cf0f89970c118721539e1dba6bba346d49830
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2023-08-12 19:49:51 +1000

    fix: resolve:ssm-secure is... not helpful (#150)

--- infra/main-stack/deploy.sh
+++ infra/main-stack/deploy.sh
@@ -6,6 +6,12 @@ cd $(dirname "$0")
 
 STACK_NAME=mux-video-monitor
 
+ATTEND_JWT_ISSUER=$(aws ssm get-parameter --name /multiview/attend/ATTEND_JWT_ISSUER --with-decryption --query Parameter.Value --output text)
+ATTEND_JWT_AUDIENCE=$(aws ssm get-parameter --name /multiview/attend/ATTEND_JWT_AUDIENCE --with-decryption --query Parameter.Value --output text)
+
+echo ::add-mask::$ATTEND_JWT_ISSUER
+echo ::add-mask::$ATTEND_JWT_AUDIENCE
+
 aws cloudformation deploy \
   --template-file deployment.cfn.yaml \
   --stack-name "$STACK_NAME" \
@@ -14,6 +20,8 @@ aws cloudformation deploy \
     "AppDomain=$APP_DOMAIN" \
     "BackendLambdaS3Bucket=$BACKEND_LAMBDA_S3_BUCKET" \
     "BackendLambdaS3Key=$BACKEND_LAMBDA_S3_KEY" \
+    "AttendJWTIssuer=$ATTEND_JWT_ISSUER" \
+    "AttendJWTAudience=$ATTEND_JWT_AUDIENCE" \
   --capabilities CAPABILITY_IAM
 
 FRONTEND_BUCKET=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query 'Stacks[0].Outputs[?OutputKey==`FrontendWebsiteBucket`].OutputValue' --output text)
