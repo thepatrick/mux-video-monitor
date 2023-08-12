bdfaac5143ed63dd53a1c9d8114d5b61ed9ef170
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2023-08-12 18:29:56 +1000

    feat: add a /api/attend route

--- backend-lambda/src/generate-token.ts
+++ backend-lambda/src/generate-token.ts
@@ -1,15 +1,27 @@
 import { sign } from 'jsonwebtoken';
-import { jwtAudience, jwtPrivateKey } from './helpers/config';
+
+const ATTEND_JWT_PRIVATE_KEY = process.env.ATTEND_JWT_PRIVATE_KEY;
+const ATTEND_JWT_AUDIENCE = process.env.ATTEND_JWT_AUDIENCE;
+const ATTEND_JWT_ISSUER = process.env.ATTEND_JWT_ISSUER;
+
+if (!ATTEND_JWT_PRIVATE_KEY) {
+  throw new Error('ATTEND_JWT_PRIVATE_KEY not set');
+}
+if (!ATTEND_JWT_AUDIENCE) {
+  throw new Error('ATTEND_JWT_AUDIENCE not set');
+}
+if (!ATTEND_JWT_ISSUER) {
+  throw new Error('ATTEND_JWT_ISSUER not set');
+}
 
 console.log(
   sign(
     {
-      iss: jwtAudience,
-      aud: jwtAudience,
-      sub: 'xxx',
-      name: 'Patrick',
+      iss: ATTEND_JWT_AUDIENCE,
+      aud: ATTEND_JWT_ISSUER,
+      sub: 'generate-token.ts',
     },
-    jwtPrivateKey,
-    { expiresIn: '24h' },
+    ATTEND_JWT_PRIVATE_KEY,
+    { expiresIn: '7d', algorithm: 'HS256' },
   ),
 );
--- backend-lambda/src/types.ts
+++ backend-lambda/src/types.ts
@@ -2,3 +2,10 @@
 export interface MuxWebhookBody {
   type: string;
 }
+
+/** @see {isDecodedJWT} ts-auto-guard:type-guard */
+export interface DecodedJWT {
+  iss: string;
+  aud: string;
+  sub: string;
+}
